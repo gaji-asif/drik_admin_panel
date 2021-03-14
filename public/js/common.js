cdc1cd2abcc5fced0fd0a2039a815bc57978688a
gaji asif <asif01665@yahoo.com>
2021-03-14 17:21:39 +0600

    all

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:8080/drik";
+const baseUrl = "http://localhost/drik";
 
 const preLoader = $("\t<div class=\"theme-loader\">\n" +
     "\t\t<div class=\"loader-track\">\n" +
