1efa5686840f3641b0294658e5cf56280cf88d58
gaji asif <asif01665@yahoo.com>
2021-04-30 20:11:33 +0600

    all

--- resources/views/backEnd/images/index.blade.php
+++ resources/views/backEnd/images/index.blade.php
@@ -23,23 +23,34 @@
     <div class="card-header">
         <h5>All Upload Images</h5>
     </div>
-<div class="row">
-@if(isset($images))
+    @if(isset($images))
+<div class="row col-lg-12">
+
 @foreach($images as $image)
     <div class="card-block col-lg-3">
      <div class="card">
+        <p style="margin-top: 15px;"><input type="checkbox" name="" id=""></p>
       <img class="card-img-top" src="{{asset($image->thumbnail_url)}}" alt="Card image cap">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 class="card-title">id#{{$image->id}}</h5>
+        <p class="card-text">{{$image->title}}</p>
+        <button onclick="deleteAnImage(5)" type="button" class="btn btn-success action-icon"><i class="fa fa-check"></i></button>
+        <button onclick="deleteAnImage(5)" type="button" class="btn btn-danger action-icon"><i class="fa fa-trash-o"></i></button>
+        <button onclick="deleteAnImage(5)" type="button" class="btn btn-warning action-icon"><i class=" fa fa-certificate"></i></button>
+
     </div>
 </div>
 </div>
 
 @endforeach
-@endif
+
+
 </div>
+<div class="row" style="margin: 0 auto; margin-bottom: 15px;">
+     {!! $images->render() !!}
+</div>
+@endif
+
 </div>
 
 </div>
